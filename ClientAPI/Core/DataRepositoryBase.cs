977c07ba7baeb664f2b19ae1e675d22ecc92ea7c
Christian Felix <xiankaylle019@gmail.com>
2018-09-26 02:32:54 +0800

    added IDataRepository namesapce

--- ClientAPI/Core/DataRepositoryBase.cs
+++ ClientAPI/Core/DataRepositoryBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using ClientAPI.Data.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
